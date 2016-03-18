f005f322958660e5f6d1a2a5fa7924c105fb58f3
Aaron Hosford <aaron.hosford@ericsson.com>
2016-03-18 17:12:25 -0500

    Updated __init__.py to load plugins at import

--- attila/__init__.py
+++ attila/__init__.py
@@ -1,2 +1,18 @@
+"""
+attila
+======
+
+Automation framework.
+"""
+
+from . import notifications
+from . import emails
+from . import plugins
+
+
 __author__ = 'Aaron Hosford'
 __version__ = '0.0.0'
+
+
+# noinspection PyProtectedMember
+plugins._load_plugins()
