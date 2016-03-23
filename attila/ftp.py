f6227111951d04c1cb80d45043003ba2139f3f9f
Aaron Hosford <aaron.hosford@ericsson.com>
2016-03-23 17:57:23 -0500

    Revamped the attila.ftp module to conform to the new file system abstraction in attila.files

--- attila/ftp.py
+++ attila/ftp.py
@@ -12,52 +12,70 @@ import os
 import socket
 
 
-import attila.security
-import attila.strings
+from . import files
+from . import security
+from . import strings
+
+from .files import Path, ProxyFile
 
 
-class FTPConnectionInfo:
+DEFAULT_FTP_PORT = 21
+
+
+class FTPConnector(files.FSConnector):
     """
     Stores the FTP connection information as a single object which can then be passed around instead of using multiple
     parameters to a function.
     """
 
-    @classmethod
-    def load_from_config(cls, config, section):
-        """
-        Load the FTP connection info from a section in a config file.
+    def __init__(self, server=None, credential=None, passive=True, initial_cwd=None):
+        if server is None:
+            port = DEFAULT_FTP_PORT
+        else:
+            assert isinstance(server, str)
+            assert server
+            server, port = strings.split_port(server, DEFAULT_FTP_PORT)
+        if credential is not None:
+            assert credential.user and credential.password
+        assert passive == bool(passive)
 
-        :param config: A configparser.ConfigParser instance.
-        :param section: The section to load the connection info from.
-        :return: An FTPConnectionInfo instance.
-        """
+        super().__init__(ftp_connection, initial_cwd)
 
+        self._server = server
+        self._port = port
+        self._credential = credential if credential else None
+        self._passive = passive
+
+    def is_configured(self):
+        return self._server is not None and self._credential is not None
+
+    def configure(self, config, section):
         assert isinstance(config, configparser.ConfigParser)
+        assert section and isinstance(section, str)
+
+        super().configure(config, section)
 
         config_section = config[section]
 
         server = config_section['Server']
         passive = config.getboolean(section, 'Passive', fallback=True)
 
-        # This has to be imported here because the security module depends on this one.
-        credential = attila.security.Credential.load_from_config(config, section)
+        credential = security.Credential.load_from_config(config, section)
 
-        return cls(server, credential, passive)
-
-    def __init__(self, server, credential, passive=True):
-        assert isinstance(server, str)
-        assert server
-
-        assert credential.user and credential.password
-
-        assert passive == bool(passive)
-
-        server, port = attila.strings.split_port(server, 21)
-
-        self._server = server
-        self._port = port
-        self._credential = credential if credential else None
+        self._server, self._port = strings.split_port(server, DEFAULT_FTP_PORT)
         self._passive = passive
+        self._credential = credential
+
+    def __repr__(self):
+        server_string = None
+        if self._server is not None:
+            server_string = self._server
+            if self._port != DEFAULT_FTP_PORT:
+                server_string += ':' + str(self._port)
+        args = [repr(server_string), repr(self._credential)]
+        if not self._passive:
+            args.append('passive=False')
+        return type(self).__name__ + '(' + ', '.join(args) + ')'
 
     @property
     def server(self):
@@ -75,215 +93,193 @@ class FTPConnectionInfo:
     def passive(self):
         return self._passive
 
-    def connect(self):
-        return FTPConnection(self)
-
-    def __repr__(self):
-        args = [repr(self._server + ':' + str(self._port)), repr(self._credential)]
-        if not self._passive:
-            args.append('passive=False')
-        return type(self).__name__ + '(' + ', '.join(args) + ')'
+    def connection(self):
+        return super().connection()
 
 
-class FTPTempWD:
-    """
-    All this class does is temporarily change the working directory of an FTP connection, and then change it back. It's
-    meant for use in a with statement. It only exists to allow FTPConnection to implement the
-    temporary_working_directory(path) method.
-    """
+# noinspection PyPep8Naming,PyAbstractClass
+class ftp_connection(files.fs_connection):
 
-    def __init__(self, connection, folder):
-        assert isinstance(connection, FTPConnection)
-        assert isinstance(folder, str)
-        self.connection = connection
-        self.folder = folder
-        self.previous_folder = None
-
-    def __enter__(self):
-        self.previous_folder = None
-        if self.folder:
-            previous_folder = self.connection.getcwd()
-            if previous_folder != self.folder:
-                self.previous_folder = previous_folder
-                self.connection.chdir(self.folder)
-        return self
-
-    # noinspection PyUnusedLocal
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.previous_folder is not None and self.connection.getcwd() != self.previous_folder:
-            self.connection.chdir(self.previous_folder)
-        return False  # Do not suppress exceptions.
-
-
-# TODO: Make this inherit from attila.files.FileSystemConnection, and we will instantly get many of the file-system
-#       operations implemented there for free.
-class FTPConnection:
-
-    def __init__(self, connection_info):
+    def __init__(self, connector):
         """
-        Create a new FTPConnection instance.
+        Create a new ftp_connection instance.
 
         Example:
             # Get a connection to the FTP server.
-            connection = FTPConnection(connection_info)
+            connection = ftp_connection(connector)
         """
+        assert isinstance(connector, FTPConnector)
+        super().__init__(connector)
 
-        assert isinstance(connection_info, FTPConnectionInfo)
-
-        self._connection_info = connection_info
         self._session = None
-
-        self.open()
-
-    @property
-    def connection_info(self):
-        return self._connection_info
 
     @property
     def is_open(self):
-        if self._session is None:
-            return False
-        try:
-            self._session.voidcmd('NOOP')
-        except (socket.error, IOError):
-            # noinspection PyBroadException
+        if self._is_open:
             try:
-                self.close()
-            except Exception:
-                pass
-        return self._session is not None
+                self._session.voidcmd('NOOP')
+            except (socket.error, IOError):
+                # noinspection PyBroadException
+                try:
+                    self.close()
+                except Exception:
+                    pass
+                self._is_open = False
+        return super().is_open
 
     def open(self):
-        if self.is_open:
-            return
+        assert not self.is_open
 
-        user, password = self._connection_info.credential
+        user, password = self._connector.credential
 
         self._session = ftplib.FTP()
-        self._session.set_pasv(self._connection_info.passive)
-        self._session.connect(self._connection_info.server, self._connection_info.port)
+        self._session.set_pasv(self._connector.passive)
+        self._session.connect(self._connector.server, self._connector.port)
         self._session.login(user, password)
 
     def close(self):
         """Close the FTP connection"""
-        if self.is_open:
-            # noinspection PyBroadException
-            try:
-                self._session.quit()  # The polite way
-            except Exception:
-                self._session.close()  # The rude way
-            finally:
-                self._session = None  # The close your eyes and pretend way
-
-    def temporary_working_directory(self, folder):
-        """
-        Use with a 'with' statement to temporarily change the current working directory, and then automatically change
-        it back afterward even if there's an error. Note that simply calling this function does nothing, if you don't
-        use it within a 'with' statement.
-
-        Example Usage:
-            with connection.temporary_working_directory(folder_path):
-                # Do stuff with CWD == folder_path.
-            # Now CWD is set back to whatever it was before.
-
-        :param folder: The folder to temporarily set as the working directory.
-        :return: A context manager, usable in a 'with' statement.
-        """
-        return FTPTempWD(self, folder)
-
-    def getcwd(self):
-        assert self.is_open
-        return self._session.pwd()
-
-    def chdir(self, folder):
-        assert self.is_open
-        self._session.cwd(folder)
-
-    def __enter__(self):
-        return self  # The connection is automatically opened when it's created, so there's nothing to do.
-
-    # noinspection PyUnusedLocal
-    def __exit__(self, exc_type, exc_value, exc_tb):
-        self.close()
-        return False  # Do not suppress exceptions
-
-    def download(self, remote_path, local_path=None, overwrite=False):
-        assert self.is_open
-
-        dir_path, file_name = os.path.split(remote_path)
-
-        if local_path is None:
-            local_path = file_name
-
-        if not overwrite and os.path.exists(local_path):
-            raise FileExistsError(local_path)
-
-        with self.temporary_working_directory(dir_path):
-            with open(local_path, 'wb') as local_file:
-                self._session.retrbinary("RETR " + file_name, local_file.write)
-
-    def upload(self, local_path, remote_path=None, overwrite=False):
-        assert self.is_open
-
-        if remote_path is None:
-            remote_path = os.path.basename(local_path)
-
-        dir_path, file_name = os.path.split(remote_path)
-
-        with self.temporary_working_directory(dir_path):
-            if not overwrite and file_name in self.list_files():
-                raise FileExistsError(remote_path)
-            with open(local_path, 'rb') as local_file:
-                self._session.retrbinary("STOR " + file_name, local_file)
-
-    def list_files(self, folder=None):
-        assert self.is_open
-
-        with self.temporary_working_directory(folder or ''):
-            try:
-                return self._session.nlst()
-            except Exception as exc:
-                if '550 No files found.' in str(exc):
-                    return []
-                raise
-
-    def get_size(self, path):
-        assert self.is_open
-
-        dir_path, file_name = os.path.split(path)
-        with self.temporary_working_directory(dir_path):
-            return self._session.size(file_name)
-
-    def remove(self, path):
-        assert self.is_open
-
-        dir_path, file_name = os.path.split(path)
-        with self.temporary_working_directory(dir_path):
-            self._session.delete(file_name)
-
-    def rename(self, path, new_name):
-        assert self.is_open
-
-        dir_path, file_name = os.path.split(path)
-        with self.temporary_working_directory(dir_path):
-            self._session.rename(file_name, new_name)
-
-    def is_dir(self, path):
         assert self.is_open
 
         # noinspection PyBroadException
         try:
-            with self.temporary_working_directory(path):
+            self._session.quit()  # The polite way
+        except Exception:
+            self._session.close()  # The rude way
+        finally:
+            self._session = None  # The close your eyes and pretend way
+            self._is_open = False
+
+    @property
+    def cwd(self):
+        assert self.is_open
+        return Path(self._session.pwd(), self)
+
+    @cwd.setter
+    def cwd(self, path):
+        assert self.is_open
+        path = self.check_path(path)
+        self._session.cwd(path)
+
+    def _download(self, remote_path, local_path):
+        assert self.is_open
+        remote_path = self.check_path(remote_path)
+        assert isinstance(local_path, str)
+
+        dir_path, file_name = os.path.split(remote_path)
+
+        with Path(dir_path, self):
+            with open(local_path, 'wb') as local_file:
+                self._session.retrbinary("RETR " + file_name, local_file.write)
+
+    def _upload(self, local_path, remote_path):
+        assert self.is_open
+        assert isinstance(local_path, str)
+        remote_path = self.check_path(remote_path)
+
+        dir_path, file_name = os.path.split(remote_path)
+
+        with Path(dir_path, self):
+            with open(local_path, 'rb') as local_file:
+                self._session.retrbinary("STOR " + file_name, local_file)
+
+    def open_file(self, path, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True,
+                  opener=None):
+        """
+        Open the file.
+
+        :param path: The path to operate on.
+        :param mode: The file mode.
+        :param buffering: The buffering policy.
+        :param encoding: The encoding.
+        :param errors: The error handling strategy.
+        :param newline: The character sequence to use for newlines.
+        :param closefd: Whether to close the descriptor after the file closes.
+        :param opener: A custom opener.
+        :return: The opened file object.
+        """
+        assert self.is_open
+
+        mode = mode.lower()
+        path = self.check_path(path)
+
+        # We can't work directly with an FTP file. Instead, we will create a temp file and return it as a proxy.
+        temp_path = files.local_fs_connection.get_temp_file_path(self.name(path))
+
+        # If we're not truncating the file, then we'll need to copy down the data.
+        if mode not in ('w', 'wb'):
+            self._download(path, temp_path)
+
+        if mode in ('r', 'rb'):
+            writeback = None
+        else:
+            writeback = self._upload
+
+        return ProxyFile(Path(path, self), mode, buffering, encoding, errors, newline, closefd, opener,
+                         proxy_path=temp_path, writeback=writeback)
+
+    def list(self, path, pattern='*'):
+        assert self.is_open
+        path = Path(self.check_path(path), self)
+        with path:
+            try:
+                listing = self._session.nlst()
+            except Exception as exc:
+                # Some FTP servers give an error if the directory is empty.
+                if '550 No files found.' in str(exc):
+                    listing = []
+                else:
+                    raise
+        if pattern == '*':
+            return listing
+        else:
+            pattern = strings.glob_to_regex(pattern)
+            return [name for name in listing if pattern.match(name)]
+
+    def size(self, path):
+        assert self.is_open
+        path = self.check_path(path)
+
+        dir_path, file_name = os.path.split(path)
+        with Path(dir_path, self):
+            return self._session.size(file_name)
+
+    def remove(self, path):
+        assert self.is_open
+        path = self.check_path(path)
+
+        dir_path, file_name = os.path.split(path)
+        with Path(dir_path, self):
+            self._session.delete(file_name)
+
+    def rename(self, path, new_name):
+        assert self.is_open
+        path = self.check_path(path)
+        assert new_name and isinstance(new_name, str)
+
+        dir_path, file_name = os.path.split(path)
+        if file_name != new_name:
+            with Path(dir_path, self):
+                self._session.rename(file_name, new_name)
+
+    def is_dir(self, path):
+        assert self.is_open
+        path = self.check_path(path)
+
+        # noinspection PyBroadException
+        try:
+            with Path(path, self):
                 return True
         except Exception:
             return False
 
     def is_file(self, path):
         assert self.is_open
+        path = self.check_path(path)
 
         # noinspection PyBroadException
         try:
-            self.get_size(path)
+            self.size(path)
             return True
         except Exception:
             return False
