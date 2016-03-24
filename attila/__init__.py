3a4f17371268ab5c966e23e0cd696c20294b165e
Aaron Hosford <aaron.hosford@ericsson.com>
2016-03-24 14:18:35 -0500

    Added __all__ and cleaned up imports

--- attila/__init__.py
+++ attila/__init__.py
@@ -5,14 +5,46 @@ attila
 Automation framework.
 """
 
-from . import notifications
+from . import abc
+from . import db
 from . import emails
+from . import env
+from . import error_handling
+from . import fs
+from . import notifications
 from . import plugins
+from . import processes
+from . import progress
+from . import resources
+from . import security
+from . import strings
+from . import threads
+from . import utility
+from . import windows
 
 
 __author__ = 'Aaron Hosford'
 __version__ = '0.0.0'
 
+__all__ = [
+    'abc',
+    'db',
+    'emails',
+    'env',
+    'error_handling',
+    'fs',
+    'notifications',
+    'plugins',
+    'processes',
+    'progress',
+    'resources',
+    'security',
+    'strings',
+    'threads',
+    'utility',
+    'windows',
+]
+
 
 # noinspection PyProtectedMember
-plugins._load_plugins()
+plugins.load_plugins()
