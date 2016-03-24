1489d06cce8e75668ec8df1faeeaaae1e103b7ed
Aaron Hosford <aaron.hosford@ericsson.com>
2016-03-24 14:11:59 -0500

    Added attila.db sub-package

---
+++ attila/db/__init__.py
@@ -0,0 +1,19 @@
+"""
+attila.db
+=========
+
+Database-related functionality
+"""
+from ..abc.sql import RecordSet, sql_connection
+from ..abc.transactions import transactional_connection
+from . import adodb
+from . import sqlite
+
+
+__all__ = [
+    'RecordSet',
+    'sql_connection',
+    'transactional_connection',
+    'adodb',
+    'sqlite',
+]
