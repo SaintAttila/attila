eb5194d9a2cf1595102b00c30ae0a8ccd1195dc0
Aaron Hosford <aaron.hosford@ericsson.com>
2016-03-24 12:44:16 -0500

    Revamped attila.adodb to conform to the abstractions in attila.abc.connections, .configuration, .sql, and .transactions

--- attila/adodb.py
+++ attila/adodb.py
@@ -6,13 +6,14 @@ ADODB database interface for Python
 """
 
 import configparser
-
+from collections import OrderedDict
 
 import win32com.client
 
-
-from . import configuration
-from . import connections
+from .abc import connections
+from .abc import configuration
+from .abc import sql
+from .abc import transactions
 
 
 class Constants:
@@ -42,33 +43,25 @@ class Constants:
     adStateFetching = 8
 
 
-class RecordSet:
+class ADODBRecordSet(sql.RecordSet):
     """
-    A RecordSet is returned whenever a query is executed. It provides an interface to the selected data.
+    An ADODBRecordSet is returned whenever a query is executed. It provides an interface to the selected data.
     """
 
     def __init__(self, com_object):
         self._com_object = com_object
 
-    def __iter__(self):
-        while not (self._com_object.EOF or self._com_object.BOF):
-            yield self._com_object.Fields
-            self._com_object.MoveNext()
+    def _next(self):
+        if self._com_object.EOF or self._com_object.BOF:
+            raise StopIteration()
 
-    def _convert(self, to_string):
-        if to_string:
-            for fields in self:
-                yield [('' if field is None else str(field)) for field in fields]
-        else:
-            for fields in self:
-                yield list(fields)
+        # We should never expose the raw COM object at all. Grab the values out and convert them to an ordered
+        # dictionary instead. An ordered dictionary is just like an ordinary dict, except that when you iterate
+        # over its keys, values, or items, the original order is preserved.
+        result = OrderedDict((field.Name, field.Value) for field in self._com_object.Fields)
 
-    def convert(self, to_string=True, to_list=True):
-        results = self._convert(to_string)
-        if to_list:
-            return list(results)
-        else:
-            return results
+        self._com_object.MoveNext()
+        return result
 
 
 class ADODBConnector(connections.Connector, configuration.Configurable):
@@ -76,31 +69,6 @@ class ADODBConnector(connections.Connector, configuration.Configurable):
     Stores the ADODB connection information for a database as a single object which can then be passed around instead of
     using multiple parameters to a function. Use str(connector) to get the actual connection string.
     """
-
-    @classmethod
-    def load_from_config(cls, config, section):
-        """
-        Load the ADODB connection info from a section in a config file.
-
-        :param config: A configparser.ConfigParser instance.
-        :param section: The section to load the connection info from.
-        :return: A ADODBConnector instance.
-        """
-
-        assert isinstance(config, configparser.ConfigParser)
-
-        config_section = config[section]
-
-        server = config_section['Server']
-        database = config_section['Database']
-        driver = config_section.get('Driver')
-        trusted = config.getboolean(section, 'Trusted', fallback=None)
-
-        # This has to be imported here because the security module depends on this one.
-        import attila.security
-        credential = attila.security.Credential.load_from_config(config, section)
-
-        return cls(server, database, driver, credential, trusted)
 
     def __init__(self, server=None, database=None, driver=None, credential=None, trusted=None):
         if server is not None:
@@ -143,14 +111,14 @@ class ADODBConnector(connections.Connector, configuration.Configurable):
 
         server = config_section['Server']
         database = config_section['Database']
-        driver = config_section.get('Driver')
+        driver = config_section.get('Driver', self._driver)
         trusted = config.getboolean(section, 'Trusted', fallback=None)
 
         # This has to be imported here because the security module depends on this module, and we would get an import
         # cycle otherwise.
         import attila.security
 
-        # TODO: This assumes a credential will be loaded. What if we don't need one?
+        # If no credentials are specified, this will simply return an empty credential.
         credential = attila.security.Credential.load_from_config(config, section)
 
         assert server
@@ -215,9 +183,8 @@ class ADODBConnector(connections.Connector, configuration.Configurable):
         return type(self).__name__ + '(' + ', '.join(args) + ')'
 
 
-# TODO: Finish revamping this to conform to the connections.connection abstraction.
 # noinspection PyPep8Naming
-class adodb_connection(connections.connection):
+class adodb_connection(sql.sql_connection, transactions.transactional_connection):
 
     def __init__(self, connector, command_timeout=None, connection_timeout=None, auto_reconnect=True,
                  cursor_location=None):
@@ -238,13 +205,13 @@ class adodb_connection(connections.connection):
 
         # The command timeout is how long it takes to complete a command.
         if command_timeout is None:
-            self._command_timeout = 10 * 60  # Default is 10 minutes
+            self._command_timeout = 60 * 60  # Default is 1 hour
         else:
             self._command_timeout = command_timeout
 
         # The connection timeout is how long we're allowed to take to *establish* a connection.
         if connection_timeout is None:
-            self._connection_timeout = 30  # Default is 30 seconds
+            self._connection_timeout = 60  # Default is 1 minute
         else:
             self._connection_timeout = connection_timeout
 
@@ -252,16 +219,16 @@ class adodb_connection(connections.connection):
 
         self._cursor_location = cursor_location
 
-        self.open()
-
     @property
     def is_open(self):
         return self._com_object is not None and bool(self._com_object.State & Constants.adStateOpen)
 
     def open(self):
+        super().open()
+
         # The state may have other bits set, but we only care about the one that indicates whether it's open or not.
         if self._com_object is not None and (self._com_object.State & Constants.adStateOpen):
-            return  # If it's open_file already, do nothing.
+            return  # If it's open already, do nothing.
 
         self._com_object = win32com.client.Dispatch("ADODB.adodb_connection")
 
@@ -289,40 +256,7 @@ class adodb_connection(connections.connection):
         """Rollback the current transaction."""
         return self._com_object.RollbackTrans()
 
-    def execute_stored_procedure(self, name, *parameters):
-        """
-        Execute a stored procedure. The stored procedure can dump return data to a results table to be queried later on
-        or converted to read depending on how the stored procedure handles its data.
-
-        Example:
-            # Execute a stored procedure with 2 parameters from an open_file connection.
-            connection.execute_stored_procedure(stored_procedure_name, year_str, month_str)
-
-        :param name: The name of the stored procedure to execute.
-        :param parameters: Additional parameters to be passed to the stored procedure.
-        """
-
-        command = "Exec " + name
-        if parameters:
-            command += ' ' + ', '.join('@' + str(parameter)for parameter in parameters)
-
-        return self.execute(command)
-
-    def execute(self, command):
-        """
-        Execute a SQL command or query. If a result table is generated, it is returned as an iterator over the records.
-        Otherwise None is returned.
-
-        :param command: The SQL command to execute.
-        :return: A RecordSet instance (for queries) or None.
-        """
-        results = self.execute_raw(command)[0]
-        if results:
-            return RecordSet(results)
-        else:
-            return None
-
-    def execute_raw(self, command):
+    def _execute_raw(self, command):
         """
         Execute a SQL command or query. The results of the underlying ADODB call are returned as-is.
 
@@ -341,3 +275,36 @@ class adodb_connection(connections.connection):
                 raise
         else:
             return self._com_object.Execute(command)
+
+    def _execute(self, command):
+        """
+        Execute a SQL command or query. If a result table is generated, it is returned as an iterator over the records.
+        Otherwise None is returned.
+
+        :param command: The SQL command to execute.
+        :return: A ADODBRecordSet instance (for queries) or None.
+        """
+        results = self._execute_raw(command)[0]
+        if results:
+            return ADODBRecordSet(results)
+        else:
+            return None
+
+    def _call(self, name, *parameters):
+        """
+        Execute a stored procedure. The stored procedure can dump return data to a results table to be queried later on
+        or converted to read depending on how the stored procedure handles its data.
+
+        Example:
+            # Execute a stored procedure with 2 parameters from an open connection.
+            connection.call(stored_procedure_name, year_str, month_str)
+
+        :param name: The name of the stored procedure to execute.
+        :param parameters: Additional parameters to be passed to the stored procedure.
+        """
+
+        command = "Exec " + name
+        if parameters:
+            command += ' ' + ', '.join('@' + str(parameter) for parameter in parameters)
+
+        return self._execute(command)
