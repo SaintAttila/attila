ec928e4b40e436f136b0548b72b47a4acc7f34f6
Aaron Hosford <aaron.hosford@ericsson.com>
2016-04-25 16:32:03 -0500

    Cleaned up generate_docs.bat

--- generate_docs.bat
+++ generate_docs.bat
@@ -1,12 +1,13 @@
-@REM See http://www.sphinx-doc.org/en/stable/man/sphinx-apidoc.html
+@ECHO OFF
+REM See http://www.sphinx-doc.org/en/stable/man/sphinx-apidoc.html
 
-@pip install Sphinx
-@sphinx-apidoc -d 3 -e -f -F -H "Saint Attila" -A "Aaron Hosford" -V 0.0 -o doc/sphinx attila
-@cd doc/sphinx
+pip install Sphinx
+sphinx-apidoc -d 3 -e -f -F -H "Saint Attila" -A "Aaron Hosford" -V 0.0 -o doc/sphinx attila
+cd doc/sphinx
 
-@REM We have to run it indirectly via Python because make somehow closes cmd.exe otherwise, which
-@REM prevents the remaining commands from executing.
-@python -c "import os; os.system('make html')"
+REM We have to run it indirectly via Python because make somehow closes cmd.exe otherwise, which
+REM prevents the remaining commands from executing.
+python -c "import os; os.system('make html')"
 
-@cd ..\..
-@start doc/sphinx/_build/html/index.html
+cd ..\..
+start doc/sphinx/_build/html/index.html
