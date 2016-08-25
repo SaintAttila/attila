8419821d2a82b70b178144a1ea87d2f7a8b7bfa6
EAARHOS <aaron.hosford@ericsson.com>
2016-08-25 14:39:11 -0500

    Bug fix: Added import for sql submodule

--- attila/notifications/__init__.py
+++ attila/notifications/__init__.py
@@ -3,7 +3,7 @@ Built-in notification-related types and instances.
 """
 
 
-from . import callbacks, composites, emails, files, logs, null
+from . import callbacks, composites, emails, files, logs, null, sql
 
 
 __author__ = 'Aaron Hosford'
