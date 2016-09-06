6f9051e5ab9ebbfa9f60f8136946e89d9d8865bd
Aaron Hosford <aaron.hosford@ericsson.com>
2016-09-06 11:18:20 -0500

    Added MutexNotifier

--- attila/notifications/__init__.py
+++ attila/notifications/__init__.py
@@ -3,7 +3,7 @@ Built-in notification-related types and instances.
 """
 
 
-from . import callbacks, composites, emails, files, logs, null, sql
+from . import callbacks, composites, emails, files, logs, mutexes, null, sql
 
 
 __author__ = 'Aaron Hosford'
