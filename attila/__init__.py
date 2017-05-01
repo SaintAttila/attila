6999adb946313d42cc25299866515dde2259b5c2
Aaron Hosford <aaron.hosford@ericsson.com>
2017-05-01 14:19:43 -0500

    Version 1.10.1

--- attila/__init__.py
+++ attila/__init__.py
@@ -7,7 +7,7 @@ from . import abc, db, fs, notifications, security
 from . import configurations, context, exceptions, plugins, strings
 
 
-__version__ = '1.10'
+__version__ = '1.10.1'
 
 __author__ = 'Aaron Hosford'
 __author_email__ = 'Aaron.Hosford@Ericsson.com'
